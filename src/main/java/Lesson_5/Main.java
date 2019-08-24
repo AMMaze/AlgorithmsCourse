9d2889db7985387a9539fddf20e47faaaf63ea5e
Marat Abbas <unlock96@gmail.com>
2019-08-24 03:58:08 +0300

    recursive bag problem + result items

--- src/main/java/Lesson_5/Main.java
+++ src/main/java/Lesson_5/Main.java
@@ -1,5 +1,10 @@
 package Lesson_5;
 
+import Lesson_3.stack.Stack;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +14,10 @@ public class Main {
         items[2] = new Item("Аптечка", 4, 1500);
         items[3] = new Item("Ноутбук", 2, 40000);
         items[4] = new Item("Котелок", 1, 500);
-        System.out.println(bagOfItems(3, 0, 0, items));
+        Pair<Integer, List<Item>> result = bagOfItems(5, 0, 0, items);
+        System.out.println(result.first);
+        for (Item it : result.second)
+            System.out.println(it.name + " " + it.weight + " " + it.cost);
     }
 
     static int pow (int base, int power) {
@@ -29,14 +37,31 @@ public class Main {
         public int weight, cost;
     }
 
-    public static int bagOfItems (int bagCapacity, int cost, int idx, Item... items) {
+    static class Pair<T, E> {
+        public T first;
+        public E second;
+
+        public Pair(T first, E second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    public static Pair<Integer, List<Item>> bagOfItems (int bagCapacity, int cost, int idx, Item... items) {
         int maxCost = cost;
+        List<Item> placedItems = new LinkedList<>();
         for (int i = idx; i < items.length; i++) {
             int newCapacity = bagCapacity - items[i].weight;
-            int newCost = newCapacity >= 0 ? bagOfItems(newCapacity, cost + items[i].cost, i+1, items) : 0;
-            maxCost = Math.max(newCost, maxCost);
+            if (newCapacity < 0)
+                continue;
+            Pair<Integer, List<Item>> newValues = bagOfItems(newCapacity, cost + items[i].cost, i+1, items);
+            if (newValues.first > maxCost) {
+                maxCost = newValues.first;
+                placedItems = newValues.second;
+                placedItems.add(items[i]);
+            }
         }
-        return maxCost;
+        return new Pair<>(maxCost, placedItems);
     }
 
 }
